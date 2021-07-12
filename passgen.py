07fd3696ed115ef94f3d04a0277bf9309b076de5
latdevelopment <51201995+latdevelopment@users.noreply.github.com>
2021-07-12 16:20:11 +0000

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,1 +1,16 @@
+# Simple Password Generator
+import string
+import random
 
+passwd = ''
+def generate_password(length=8, addsyms=True, addnums=True):
+    passwd = ''
+    nums = ''
+    syms = ''
+    if addsyms:
+        syms = string.punctuation
+    if addnums:
+        nums = string.digits
+    for i in range(length):
+        passwd += random.choice(string.ascii_letters + nums + syms)
+    return passwd
