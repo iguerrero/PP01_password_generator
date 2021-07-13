e205308cadc888322f284f3db96c70dbaece1748
latdev <latdev@protonmail.com>
2021-07-12 23:42:47 -0300

    documenting the function

--- passgen.py
+++ passgen.py
@@ -5,6 +5,11 @@ import random
 passwd = ''
 
 def generate_password(length=8, addsyms=True, addnums=True):
+    '''
+    Generates by default an 8 characters long password. You can change the length of the password
+    and choose whether you want numbers or symbols.
+    '''
+
     passwd = ''
     nums = ''
     syms = ''
@@ -12,6 +17,9 @@ def generate_password(length=8, addsyms=True, addnums=True):
         syms = string.punctuation
     if addnums:
         nums = string.digits
-    for i in range(length):
+    for _ in range(length):
         passwd += random.choice(string.ascii_letters + nums + syms)
     return passwd
+
+if __name__=='__main__':
+    generate_password()
